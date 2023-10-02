ae22fbd8dd63571b5ded60adda0d9e82a63525d2
Seth Fogarty <sfogarty@trinity.edu>
2023-10-02 12:02:19 -0500

    push

--- lec18_fold_exercises.hs
+++ lec18_fold_exercises.hs
@@ -9,7 +9,7 @@ map :: (a -> b) -> [a] -> [b]
 filter :: (a -> Bool) -> [a] -> [a]
 -}
 
--- 1) Using a fold, rewrite myAnd :: [Bool] -> Bool 
+-- 1) Rewrite rewrite myAnd :: [Bool] -> Bool using a fold.
 myAnd :: [Bool] -> Bool
 myAnd [] = True
 myAnd (x:xs) = x && (myAnd xs)
@@ -18,39 +18,39 @@ myAnd (x:xs) = x && (myAnd xs)
 positives :: [Int] -> [Int]
 positives = undefined
 
--- 3) Using a fold, define range :: [Int] -> (Int,Int) 
-range :: [Int] -> (Int, Int)
-range lst = (min lst, max lst)
-
--- 4) Redefine zeroDetector :: [Int] -> Bool using a fold
+-- 3) Rewrite zeroDetector :: [Int] -> Bool using a fold
 zeroDetector :: [Int] -> Bool
 zeroDetector lst = not $ null $ filter (==0) lst
 
--- 5) Redefine splitOnParity :: [Int] -> ([Int], [Int]) using a fold instead of filters.
+-- 4) Rewrite splitOnParity :: [Int] -> ([Int], [Int]) using a fold instead of filters.
 splitOnParity :: [Int] -> ([Int], [Int])
 splitOnParity lst = (filter even lst, filter odd lst)
 
--- 6) Define sumValues :: [(a, Int)] -> Int that sums the values of an association list.  
+-- 5) Define sumValues :: [(a, Int)] -> Int that sums the values of an association list.  
 sumValues :: [(a, Int)] -> Int
 sumValues = undefined
 
--- 7) Define bestKey :: [(a, Int)] -> a that finds the key with the maximum value.
+-- 6) Define bestKey :: [(a, Int)] -> a that finds the key with the maximum value.
 bestKey :: [(a, Int)] -> a
 bestKey = undefined
 
--- 8) Define a higher-order function exists :: (a -> Bool) -> [a] -> Bool that takes a predicate, a
+-- 7) Define a higher-order function exists :: (a -> Bool) -> [a] -> Bool that takes a predicate, a
 -- list, and returns true if the predicate is true for anything in the list. 
    -- a) Using recursion.
    -- b)(Optional) using a fold.
 exists :: (a -> Bool) -> [a] -> Bool
 exists = undefined
 
--- 9) Rewrite zeroDetector using only exists.
+-- 8) Rewrite zeroDetector using only exists.
 zeroDetector2 :: [Int] -> Bool
 zeroDetector2 = undefined
 
--- 10) Using exists, define isPrime :: Int -> Bool
+-- 9) Using exists, define isPrime :: Int -> Bool
 isPrime :: Int -> Bool
 isPrime = undefined
 -- Hint: First define isMultiple :: Int -> Int -> Bool that takes two Ints and returns True if the
 -- first can be divided by the second.
+
+-- 10) Rewrite range :: [Int] -> (Int,Int) using a single fold
+range :: [Int] -> (Int, Int)
+range lst = (minimum lst, maximum lst)
